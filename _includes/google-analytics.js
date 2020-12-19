584db1a95045e72dee1634b299321f2dd8ff275c
Chubak <chubakbidpaa@gmail.com>
2020-12-19 09:54:12 +0330

    ga new

--- _includes/google-analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7D7LDCPQ9Z"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-7D7LDCPQ9Z');
-</script>
